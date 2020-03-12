db723396240bde9bb510c494b477e3fd81138f09
etherin@aamon <in37her@protonmail.com>
2020-03-12 21:00:50 +0700

    Remove await in never awaiting functions

--- app/routes.py
+++ app/routes.py
@@ -13,8 +13,8 @@ def get_boards(db: Session = Depends(get_db)):
 
 @app.post("/api/board", response_model=schemes.Board, response_model_exclude={"board_id"},
           tags=["Board"])
-async def add_board(board: schemes.BoardBase, db: Session = Depends(get_db)):
-    return await crud.create_board(db, board)
+def add_board(board: schemes.BoardBase, db: Session = Depends(get_db)):
+    return crud.create_board(db, board)
 
 
 @app.get("/api/board/{board_id}", response_model=schemes.Board, tags=["Board"])
@@ -24,17 +24,17 @@ def get_board(board_id: int, db: Session = Depends(get_db)):
 
 
 @app.delete("/api/board/{board_id}", tags=["Board"])
-async def delete_board(board_id: int, db: Session = Depends(get_db)):
+def delete_board(board_id: int, db: Session = Depends(get_db)):
     board = schemes.Board(id=board_id)
     if not crud.delete_board(db, board):
         raise HTTPException(status_code=404)
 
 
 @app.post("/api/note", response_model=schemes.Note, tags={"Note"})
-async def add_note(board_id: int, note: schemes.NoteBase, db: Session = Depends(get_db)):
+def add_note(board_id: int, note: schemes.NoteBase, db: Session = Depends(get_db)):
     if crud.get_board(db, board_id) is None:
         raise HTTPException(status_code=404, detail="Board not found")
-    return await crud.create_note(db, board_id, note)
+    return crud.create_note(db, board_id, note)
 
 
 @app.delete("/api/note/{note_id}", tags={"Note"})
